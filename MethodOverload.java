f5f4791aeccba9b65091185d38d4df3108283ca0
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-04-29 23:08:32 +0200

    Test method overloading

---
+++ MethodOverload.java
@@ -0,0 +1,42 @@
+public class MethodOverload {
+    // Demonstrate method overloading.
+    void ovlDemo() {
+        System.out.println("No parameters");
+    }
+
+    // Overload ovlDemo for one integer parameter.
+    void ovlDemo(int a) {
+        System.out.println("One parameter: " + a);
+    }
+
+    // Overload ovlDemo for two integer parameters.
+    int ovlDemo(int a, int b) {
+        System.out.println("Two parameters: " + a + " " + b);
+        return a + b;
+    }
+
+    // Overload ovlDemo for two double parameters.
+    double ovlDemo(double a, double b) {
+        System.out.println("Two double parameters: " + a + " " + b);
+        return a + b;
+    }
+}
+
+class OverloadDemo {
+    public static void main(String[] args) {
+        var ob = new MethodOverload();
+        int resI;
+        double resD;
+
+        // Call all versions of ovlDemo().
+        ob.ovlDemo();
+        System.out.println();
+        ob.ovlDemo(2);
+        System.out.println();
+        resI = ob.ovlDemo(4, 6);
+        System.out.println("Result of ob.ovlDemo(4, 6): " + resI);
+        System.out.println();
+        resD = ob.ovlDemo(1.1, 2.32);
+        System.out.println("Result of ob.ovlDemo(1.1, 2.32): " + resD);
+    }
+}
