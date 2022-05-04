995765eb852e33df9c3920ffc481f9a70cff23c2
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-05-04 21:09:33 +0200

    Implement casting

---
+++ CastingTest.java
@@ -0,0 +1,17 @@
+public class CastingTest {
+    public static void main(String[] args) {
+        // Declaring.
+        double x, y;
+        int i, n;
+        byte b;
+        char c;
+
+        // Initializing.
+        x = 10.0;
+        y = 3.0;
+        i = (int) (x / y);
+        n = (int) (x / y);
+
+        System.out.println(i + ", " + (x / y));
+    }
+}
