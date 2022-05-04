c6f3b10ad36a3a0600f422652aa10cc88faeacca
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-05-04 21:10:38 +0200

    Try nested loop

---
+++ NestedLoop.java
@@ -0,0 +1,12 @@
+public class NestedLoop {
+    public static void main(String args[]) {
+        int i, j;
+
+        for(i = 0; i < 10; i++) {
+            for(j = i; j < 10; j ++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
